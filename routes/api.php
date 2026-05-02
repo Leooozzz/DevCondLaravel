0cbc21946a6cd7907d1b5023e716cf3e94cd68a3
leooozzz <leozeraffcanal2405@gmail.com>
2026-05-02 00:04:02 -0300

    feat:logout and validate token

--- routes/api.php
+++ routes/api.php
@@ -21,8 +21,8 @@ Route::post('/auth/register', [AuthController::class, 'register']);
 
 Route::middleware('auth:api')->group(function () {
 
-  Route::post('/auht/validate', [AuthController::class, 'valideteToken']);
-  Route::post('/auht/logout', [AuthController::class, 'logout']);
+  Route::post('/auth/validate', [AuthController::class, 'valideteToken']);
+  Route::post('/auth/logout', [AuthController::class, 'logout']);
   //Notice walls
   Route::get('/walls', [WallController::class, 'getAll']);
   Route::post('/wall/{id}/like', [WallController::class, 'like']);
