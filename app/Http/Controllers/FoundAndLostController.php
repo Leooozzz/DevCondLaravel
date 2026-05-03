988daa69f0f4f8ad000853a31fdf7a444fdad664
leooozzz <leozeraffcanal2405@gmail.com>
2026-05-03 12:20:38 -0300

    feat: insert item, and change status

--- app/Http/Controllers/FoundAndLostController.php
+++ app/Http/Controllers/FoundAndLostController.php
@@ -4,6 +4,7 @@ namespace App\Http\Controllers;
 
 use App\Models\FoundAndLost;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Validator;
 
 class FoundAndLostController extends Controller
 {
@@ -27,19 +28,53 @@ class FoundAndLostController extends Controller
 
         return $array;
     }
-    public function insert()
+    public function  insert(Request $request)
     {
         $array = ['error' => ''];
 
+        $validator = Validator::make($request->all(), [
+            'description' => 'required',
+            'where' => 'required',
+            'photo' => 'required|file|mimes:jpg,png,jpeg'
+        ]);
+        if (!$validator->fails()) {
+            $description = $request->input('description');
+            $where = $request->input('where');
+            $file = $request->file('photo')->store('public');
 
+            $file = explode('public/', $file);
+            $photo = $file[1];
+            $newLost = new FoundAndLost();
+
+            $newLost->status = 'LOST';
+            $newLost->photo =  $photo;
+            $newLost->where =  $where;
+            $newLost->description =  $description;
+            $newLost->date_created = date('Y-m-d');
+            $newLost->save();
+        } else {
+            $array['error'] = $validator->errors()->first();
+            return $array;
+        }
 
         return $array;
     }
-    public function update()
+    public function update($id, Request $request)
     {
         $array = ['error' => ''];
 
-
+        $status = $request->input('status');
+        if ($status && in_array($status, ['LOST', 'RECOVERED'])) {
+            $item = FoundAndLost::find('id', $id);
+            if (!$item) {
+                $array['error'] = "Not exists";
+            }
+            $item->status = $status;
+            $item->save();
+        } else {
+            $array['error'] = "Status not exists";
+            return $array;
+        }
 
         return $array;
     }
