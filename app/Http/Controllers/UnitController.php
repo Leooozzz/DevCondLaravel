61cf4b80b66ed2cdad5b55a4f619ac52f8bd555d
leooozzz <leozeraffcanal2405@gmail.com>
2026-05-03 14:02:00 -0300

    feat: add person-vehicle-pet

--- app/Http/Controllers/UnitController.php
+++ app/Http/Controllers/UnitController.php
@@ -2,9 +2,112 @@
 
 namespace App\Http\Controllers;
 
+use App\Models\Unit;
+use App\Models\UnitPeople;
+use App\Models\UnitPet;
+use App\Models\UnitVehicle;
+use Illuminate\Contracts\Support\ValidatedData;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Validator;
 
 class UnitController extends Controller
 {
-    //
+    public function getInfo($id)
+    {
+        $array = ['error' => ''];
+
+        $unit = Unit::find('id', $id);
+        if (!$unit) {
+            $array['error'] = 'Property  not exists';
+            return $array;
+        }
+        if ($unit) {
+            $peoples = UnitPeople::query()->where('id_unit', $id)->get();
+            $vehicles = UnitVehicle::query()->where('id_unit', $id)->get();
+            $pets = UnitPet::query()->where('id_unit', $id)->get();
+
+            foreach ($peoples as $peoplesKey => $peopleValue) {
+                $peoples[$peoplesKey]['birthdate'] = date('d/m/Y', strtotime($peopleValue['birthdate']));
+            }
+            $array['peoples'] = $peoples;
+            $array['vehicles'] = $vehicles;
+            $array['pets'] = $pets;
+        }
+        return $array;
+    }
+    public function addPerson($id, Request $request)
+    {
+        $array = ['error' => ''];
+
+        $validator = Validator::make($request->all(), [
+            'name' => 'required',
+            'birthdate' => 'required|date'
+        ]);
+
+        if (!$validator->fails()) {
+            $name = $request->input('name');
+            $birthdate = $request->input('birthdate');
+
+            $newPerson = new UnitPeople();
+            $newPerson->id_unit = $id;
+            $newPerson->name = $name;
+            $newPerson->birthdate = $birthdate;
+            $newPerson->save();
+        } else {
+            $array['error'] = $validator->errors()->first();
+            return $array;
+        }
+        return $array;
+    }
+    public function addVehicle($id, Request $request)
+    {
+        $array = ['error' => ''];
+
+        $validator = Validator::make($request->all(), [
+            'title' => 'required',
+            'color' => 'required|date',
+            'plate' => 'required'
+        ]);
+
+        if (!$validator->fails()) {
+            $title = $request->input('title');
+            $color = $request->input('color');
+            $plate = $request->input('plate');
+
+            $newVehicle = new UnitVehicle();
+            $newVehicle->id_unit = $id;
+            $newVehicle->title = $title;
+            $newVehicle->color = $color;
+            $newVehicle->plate = $plate;
+            $newVehicle->save();
+        } else {
+            $array['error'] = $validator->errors()->first();
+            return $array;
+        }
+        return $array;
+    }
+    public function addPet($id, Request $request)
+    {
+        $array = ['error' => ''];
+
+        $validator = Validator::make($request->all(), [
+            'name' => 'required',
+            'race' => 'required'
+        ]);
+
+        if (!$validator->fails()) {
+            $name = $request->input('name');
+            $race = $request->input('race');
+
+            $newPet = new UnitVehicle();
+            $newPet->id_unit = $id;
+            $newPet->name = $name;
+            $newPet->race = $race;
+            $newPet->save();
+        } else {
+            $array['error'] = $validator->errors()->first();
+            return $array;
+        }
+        return $array;
+    }
 }
